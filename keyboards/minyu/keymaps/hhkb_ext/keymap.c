ae2e74c16b71bc9939405f019bead893a1951e8c
minyu <root@Gentoo-GMK>
2024-10-03 21:03:26 +0800

    双角色按键使用体验调整

--- keyboards/minyu/keymaps/hhkb_ext/keymap.c
+++ keyboards/minyu/keymaps/hhkb_ext/keymap.c
@@ -18,17 +18,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |LCtrl |  A|  S|  D|  F|  G|  H|  J|  K|  L|  :|  '|Return  |
      * |-----------------------------------------------------------|
-     * |LShift |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|RShift|Del |  RShift = Up
+     * |LShift |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|RShift|RFn |  RShift/RFn = Up/Del
      * |-----------------------------------------------------------|
-     * |LFn |LAlt|LGui |          Space          |RGui |RAlt |RFn  |  RGui/RAlt/RFn = Left Down Right
+     * |LFn |LAlt|LGui |          Space          |RGui |RAlt |RCtrl|  RGui/RAlt/RCtrl = Left/Down/Right
      * `-----------------------------------------------------------'
      */
     [_BL] = LAYOUT_60_tsangan_hhkb(
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, MT(MOD_RSFT, KC_UP), KC_DEL,
-        MO(_LFN), KC_LALT, KC_LGUI, KC_SPC, MT(MOD_RGUI, KC_LEFT), MT(MOD_RALT, KC_DOWN), LT(_RFN, KC_RGHT)),
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, MT(MOD_RSFT, KC_UP), LT(_RFN, KC_DEL),
+        MO(_LFN), KC_LALT, KC_LGUI, KC_SPC, MT(MOD_RGUI, KC_LEFT), MT(MOD_RALT, KC_DOWN), MT(MOD_RCTL, KC_RGHT)),
 
     /* HHKB Right Fn layer
      * ,-----------------------------------------------------------.
